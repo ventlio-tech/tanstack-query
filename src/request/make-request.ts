93a4f284765fd65a38fcdcbcb95e942b085886a8
stancobridge <stancoproof@gmail.com>
2023-04-30 14:05:27 +0100

    fix content type bug

--- src/request/make-request.ts
+++ src/request/make-request.ts
@@ -18,11 +18,10 @@ export async function makeRequest<TResponse>({
   body = isFormData ? buildFormData(body as Record<string, any>) : body;
 
   // configure request header
-
-  console.log({ isFormData });
-
   if (!isFormData) {
     headers['Content-Type'] = ContentType.APPLICATION_JSON;
+  } else {
+    delete headers['Content-Type'];
   }
 
   try {
