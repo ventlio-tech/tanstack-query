de0bc312837f1b8f56eb38941d9edac94cbc4a0a
stancobridge <stancoproof@gmail.com>
2023-05-10 14:41:46 +0100

    fix single file upload

--- src/request/buildFormData.ts
+++ src/request/buildFormData.ts
@@ -1,28 +1,51 @@
 export const buildFormData = (body: Record<string, any>) => {
   const formData = new FormData();
 
+  const handleArrayValue = (key: string, value: any) => {
+    for (const item of value) {
+      if (item instanceof File) {
+        formData.append(key, item);
+      } else if (item instanceof Object) {
+        formData.append(
+          key,
+          new Blob([JSON.stringify(item)], {
+            type: 'application/json',
+          })
+        );
+      } else {
+        formData.append(key, item);
+      }
+    }
+  };
+
+  const handleObjectValue = (key: string, value: any) => {
+    if (value instanceof File) {
+      formData.append(key, value);
+    } else {
+      formData.append(
+        key,
+        new Blob([JSON.stringify(value)], {
+          type: 'application/json',
+        })
+      );
+    }
+  };
+
+  const handlePrimitiveValue = (key: string, value: any) => {
+    formData.append(key, value);
+  };
+
   const bodyKeys = Object.keys(body);
 
   bodyKeys.forEach((key) => {
     const inputValue = body[key];
 
-    // if inputValue is array and the first item is array, append everything to the
-    if (inputValue instanceof Array && inputValue.length > 0) {
-      // append all files
-      for (const file of inputValue) {
-        formData.append(key, file);
-      }
-    }
-    // else if inputValue is Object upload it as blob
-    else if (inputValue instanceof Object) {
-      formData.append(
-        key,
-        new Blob([JSON.stringify(inputValue)], {
-          type: 'application/json',
-        })
-      );
+    if (Array.isArray(inputValue) && inputValue.length > 0) {
+      handleArrayValue(key, inputValue);
+    } else if (inputValue instanceof Object) {
+      handleObjectValue(key, inputValue);
     } else {
-      formData.append(key, inputValue);
+      handlePrimitiveValue(key, inputValue);
     }
   });
 
