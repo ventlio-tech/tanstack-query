76f82d39fa6a0043b6e91c8bc0bcc2e1c0ef7cad
stancobridge <stancoproof@gmail.com>
2023-05-28 12:41:22 +0000

    fix fetch bug in react native

--- src/config/useEnvironmentVariables.ts
+++ src/config/useEnvironmentVariables.ts
@@ -3,8 +3,8 @@ import { useReactNativeEnv } from './useReactNativeEnv';
 
 export const useEnvironmentVariables = (): IConfig => {
   const { appTimeout, appUrl } = useReactNativeEnv();
-  const url = process.env.REACT_APP_API_URL || process.env.NEXT_PUBLIC_API_URL || appUrl;
-  const timeout = process.env.REACT_APP_API_TIMEOUT || process.env.NEXT_PUBLIC_API_TIMEOUT || appTimeout;
+  const url = process.env.REACT_APP_API_URL ?? process.env.NEXT_PUBLIC_API_URL ?? appUrl;
+  const timeout = process.env.REACT_APP_API_TIMEOUT ?? process.env.NEXT_PUBLIC_API_TIMEOUT ?? appTimeout;
 
   return {
     API_URL: url as string,
